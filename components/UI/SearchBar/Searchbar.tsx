e9f7ec5de9ba7526a205e0c21e2ddf2dd32427e4
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-02-21 16:14:49 +0100

    finished searchbar

--- components/UI/SearchBar/Searchbar.tsx
+++ components/UI/SearchBar/Searchbar.tsx
@@ -1,11 +1,21 @@
 import Search from "/public/icons/search.svg"
-import Image from "next/image"
+import classes from "./Searchbar.module.scss"
+import classNames from "classnames"
 
-const SearchBar = () => {
+interface IPropTypes {
+	className?: string
+}
 
+const SearchBar = ({ className }: IPropTypes) => {
+	const clsx = classNames(className, classes.search)
 	return (
-		<div>
-			<Search />
+		<div className={clsx}>
+			<Search className={classes.search__icon} />
+			<input
+				type='text'
+				className={classes.search__input}
+				placeholder='Search'
+			/>
 		</div>
 	)
 }
