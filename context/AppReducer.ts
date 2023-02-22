0845e5c8079d1335a0247a572f3c18db6a8098b0
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-02-22 05:59:39 +0100

    added data fetching for index page

--- context/AppReducer.ts
+++ context/AppReducer.ts
@@ -5,6 +5,14 @@ export const AppReducer = (
 	action: ActionCreator<ActionPayload>[keyof ActionCreator<ActionPayload>]
 ): initialAppState => {
 	switch (action.type) {
+		case ACTIONS.set_app_data: {
+			return {
+				...state,
+				all_new_tracks: action.payload?.new_t,
+				all_playlists: action.payload?.playlist,
+				all_popular_tracks: action.payload?.popular,
+			}
+		}
 		default:
 			return state
 	}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,34 @@
 import Head from "next/head"
-import Image from "next/image"
+import Hero from "../components/Hero/Hero"
+import { GetServerSideProps } from "next"
+import type { Playlist, Track } from "../context/context-types"
+import { useEffect } from "react"
+import { useLocalContext } from "../context/AppContext"
+import axios from "axios"
+import { ACTIONS } from "../context/actions"
 
-export default function Home() {
+interface IProps {
+	data: {
+		playlist: Playlist[]
+		new_t: Track[]
+		popular: Track[]
+	}
+}
+
+export default function Home({ data: { playlist, new_t, popular } }: IProps) {
+	const { dispatch } = useLocalContext()
+
+	useEffect(() => {
+		dispatch({
+			type: ACTIONS.set_app_data,
+			payload: {
+				new_t,
+				playlist,
+				popular,
+			},
+		})
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 	return (
 		<>
 			<Head>
@@ -19,7 +46,29 @@ export default function Home() {
 					href='/logo.svg'
 				/>
 			</Head>
-			<div></div>
+			<Hero />
 		</>
 	)
+}
+export const getServerSideProps: GetServerSideProps<{
+	data: {
+		playlist: Playlist[]
+		new_t: Track[]
+		popular: Track[]
+	}
+}> = async context => {
+	const playlist_promise = await axios("https://musica-api.up.railway.app/playlist")
+	const playlist = playlist_promise?.data
+
+	const new_t_promise = await axios("https://musica-api.up.railway.app/new")
+	const new_t = new_t_promise?.data
+
+	const popular_promise = await axios("https://musica-api.up.railway.app/popular")
+	const popular = popular_promise?.data
+
+	return {
+		props: {
+			data: { playlist, new_t, popular },
+		},
+	}
 }
